b8d5ff5ffbc61ae3595ac910c224327f71a7ef7f
fightingggg <121657134+fightingggg@users.noreply.github.com>
2023-12-29 16:20:03 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Board from './component/Board';
-import View from './component/View';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Write from './component/Write';
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, category: '공지', title: '첫 번째 게시글', content: '내용', views: 100 },
+    // 초기 게시글 데이터...
+  ]);
+
+  const addPost = (newPost) => {
+    setPosts([newPost, ...posts]); // 새 게시글을 목록의 앞에 추가
+  };
+
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route exact path="/" element={<Board />} />
-          <Route path="/view/:postId" element={<View />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Board posts={posts} />} />
+        <Route path="/write" element={<Write addPost={addPost} />} />
+      </Routes>
     </Router>
   );
 }
